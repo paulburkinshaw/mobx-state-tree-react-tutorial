7c036d299bcd0e24ff6ac646076ff97fd953f039
Paul Burkinshaw <paul.burkinshaw@gmail.com>
2020-06-14 16:20:34 +0100

    Initial commit

---
+++ src/mst/index.ts
@@ -0,0 +1,37 @@
+import { types, Instance, applySnapshot } from 'mobx-state-tree';
+import { v4 as uuidv4 } from 'uuid';
+
+const EmployeeModel = types.model("Employee", {
+    id: types.identifier,
+    name: types.string,
+    hours_worked: types.number
+});
+
+const EmployerModel = types.model("Employer", {
+    id: types.identifier,
+    name: types.string,
+    location: types.string,
+    employees: types.array(EmployeeModel)
+})
+    .actions(self => {
+        function newEmployee(name: string, hours_worked: number) {
+            const id = uuidv4();
+            applySnapshot(self,
+                {
+                    ...self, employees: [{ id, name, hours_worked }, ...self.employees]
+                });
+        }
+        return { newEmployee };
+    })
+
+const RootModel = types.model("Root", {
+    employer: EmployerModel
+});
+
+export { RootModel };
+
+
+export type Root = Instance<typeof RootModel>;
+export type Employer = Instance<typeof EmployerModel>;
+export type Employee = Instance<typeof EmployeeModel>;
+
---
+++ src/mst/setup.ts
@@ -0,0 +1,26 @@
+import { RootModel } from "."
+import { onSnapshot, getSnapshot, applySnapshot } from "mobx-state-tree";
+
+import makeInspectable from "mobx-devtools-mst";
+
+export const setupRootStore = () => {
+    
+    const rootTree = RootModel.create({
+        employer: {
+            id: "1",
+            name: "Red Hare Consulting",
+            location: "UK",
+            employees: []
+        }
+    });
+
+    // makeInspectable(rootTree);
+
+    onSnapshot(rootTree, (snapshot) => console.log('snapshot: ', snapshot))
+
+    // dont need these, just to demonstrate onSnapshot
+    /* const currentRootTree = getSnapshot(rootTree)
+    applySnapshot(rootTree, {...currentRootTree, employer: {...currentRootTree.employer, location: "Manhattan"} }) */
+
+    return { rootTree };
+};
